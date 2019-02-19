1b8b2eb0c67cd1a064cca3eae0ae11e5896e3033
isaac <kalinkin.isaac@icloud.com>
2019-02-19 18:13:24 +0300

    cycle colors add

--- plotter/graph_plotter.py
+++ plotter/graph_plotter.py
@@ -3,25 +3,24 @@ from matplotlib import collections as mc
 from matplotlib.path import Path
 import matplotlib.patches as patches
 
+
 class GraphPlotter(object):
     FONT_SIZE = 4
+    CYCLE_COLORS = ('black', 'b', 'r')
 
     def __init__(self, ax):
         self._ax = ax
 
-
     def draw_vertex(self, xy, color='w'):
         self._ax.add_artist(patches.Circle(xy, radius=0.045, facecolor=color, edgecolor='black'))
 
-    def plot(self, graph: BCGraph):
-        margin = 0.1
+    def plot(self, graph: BCGraph, cycle_colors=CYCLE_COLORS, cycle_text=False):
         black_delta = 0.25
         height = black_delta * (len(graph.V0) + 1)
         white_delta = height / (len(graph.V1) + 1)
 
         self._ax.set_xlim(-0.2, 1.2)
         self._ax.set_ylim(height - 1.2, height + 0.2)
-
 
         black = []
         white = []
@@ -37,8 +36,9 @@ class GraphPlotter(object):
             self.draw_vertex(pos, color='w')
 
         lines = []
+        colors = []
 
-        linewidth = 1.45
+        line_width = 1.45
 
         for i in range(len(graph.V0)):
             nei = graph.V0[i]
@@ -69,24 +69,39 @@ class GraphPlotter(object):
                          Path.CURVE4,
                          ]
                 path1 = Path(vertices_1, codes)
-                self._ax.text(
-                    mid1[0],
-                    mid1[1],
-                    '1',
-                    fontsize=GraphPlotter.FONT_SIZE,
-                    bbox=dict(facecolor='w', edgecolor='black', boxstyle='round')
-                )
+                if cycle_text:
+                    self._ax.text(
+                        mid1[0],
+                        mid1[1],
+                        '1',
+                        fontsize=GraphPlotter.FONT_SIZE,
+                        bbox=dict(facecolor='w', edgecolor='black', boxstyle='round')
+                    )
                 path2 = Path(vertices_2, codes)
-                self._ax.text(
-                    mid2[0],
-                    mid2[1],
-                    '2',
-                    fontsize=GraphPlotter.FONT_SIZE,
-                    bbox=dict(facecolor='w', edgecolor='black', boxstyle='round')
+                if cycle_text:
+                    self._ax.text(
+                        mid2[0],
+                        mid2[1],
+                        '2',
+                        fontsize=GraphPlotter.FONT_SIZE,
+                        bbox=dict(facecolor='w', edgecolor='black', boxstyle='round')
+                    )
+
+                patch1 = patches.PathPatch(
+                    path1,
+                    edgecolor=cycle_colors[0],
+                    facecolor='none',
+                    linewidth=line_width,
+                    zorder=-10
                 )
 
-                patch1 = patches.PathPatch(path1, facecolor='none', linewidth=linewidth, zorder=-10)
-                patch2 = patches.PathPatch(path2, facecolor='none', linewidth=linewidth, zorder=-10)
+                patch2 = patches.PathPatch(
+                    path2,
+                    edgecolor=cycle_colors[1],
+                    facecolor='none',
+                    linewidth=line_width,
+                    zorder=-10
+                )
 
                 self._ax.add_patch(patch1)
                 self._ax.add_patch(patch2)
@@ -95,30 +110,36 @@ class GraphPlotter(object):
                     if len(graph.V1[n]) == 3:
                         if GraphPlotter.has_parallel(graph.V1[n]):
                             pos = GraphPlotter.lerp(black[i], white[n], 0.5)
-                            self._ax.text(
-                                pos[0],
-                                pos[1],
-                                '3',
-                                fontsize=GraphPlotter.FONT_SIZE,
-                                bbox=dict(facecolor='w', edgecolor='black', boxstyle='round')
-                            )
+                            if cycle_text:
+                                self._ax.text(
+                                    pos[0],
+                                    pos[1],
+                                    '3',
+                                    fontsize=GraphPlotter.FONT_SIZE,
+                                    bbox=dict(facecolor='w', edgecolor='black', boxstyle='round')
+                                )
+                            colors.append(cycle_colors[2])
                         else:
                             pos = GraphPlotter.lerp(black[i], white[n], 0.35)
-                            self._ax.text(
-                                pos[0],
-                                pos[1],
-                                str(graph.V1[n].index(i) + 1),
-                                fontsize=GraphPlotter.FONT_SIZE,
-                                bbox=dict(facecolor='w', edgecolor='black', boxstyle='round')
-                            )
+                            if cycle_text:
+                                self._ax.text(
+                                    pos[0],
+                                    pos[1],
+                                    str(graph.V1[n].index(i) + 1),
+                                    fontsize=GraphPlotter.FONT_SIZE,
+                                    bbox=dict(facecolor='w', edgecolor='black', boxstyle='round')
+                                )
+                            colors.append(cycle_colors[graph.V1[n].index(i)])
+                    else:
+                        colors.append('black')
                     lines.append([white[n], black[i]])
 
-        lc = mc.LineCollection(lines, linewidths=linewidth, colors=['black'] * len(lines), zorder=-10)
+        lc = mc.LineCollection(lines, linewidths=line_width, colors=colors, zorder=-10)
         self._ax.add_collection(lc)
 
     @staticmethod
     def lerp(start, end, t=0.5, x_shift=0.0, y_shift=0.0):
-        return ((1 - t)*start[0] + t*end[0] + x_shift, (1 - t)*start[1] + t*end[1] + y_shift)
+        return (1 - t)*start[0] + t*end[0] + x_shift, (1 - t)*start[1] + t*end[1] + y_shift
 
     @staticmethod
     def has_parallel(nei):
